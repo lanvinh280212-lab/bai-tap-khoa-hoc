f62f15e2e146bee7bb2f58a00d8775a4e2512f25
Nguyen Thi Lan <nguyenlan280204@gmail.com>
2026-07-21 13:01:01 +0700

    them bai 9

---
+++ moudle2/JS_Fundamentals_Flowchart/bai9/number.js
@@ -0,0 +1,15 @@
+let a = Number(prompt("Nhập số a: "));
+let b = Number(prompt("Nhập số b: "));
+let c = a+b;
+let d= a-b;
+let e = a*b;
+let h = a/b;
+let g = a%b;
+
+console.log(" Kết quả")
+console.log("Số a = " + a + ",Số b = " +b);
+console.log("Tổng : " + c);
+console.log("hiệu: " + d);
+console.log("Tích : " + e);
+console.log("Thương: " + h);
+console.log("Số dư: " + g);
