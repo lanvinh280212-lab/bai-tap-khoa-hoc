bed2b59cf6c4f14507c5daa8e5c4c4f93a089d57
Nguyen Thi Lan <nguyenlan280204@gmail.com>
2026-07-21 09:44:22 +0700

    them anh bai1

---
+++ moudle2/JS_Fundamentals_Flowchart/js/strictEquality.js
@@ -0,0 +1,18 @@
+let numberA= 5;
+let stringB = "5";
+if ( numberA ==  stringB) {
+    console.log("A equals B");
+} 
+else {
+    console.log("A is not equal B")
+}
+
+if ( numberA ===  stringB) {
+    console.log("A equals B")
+}
+else {
+    console.log("A is not equal B")
+}
+
+let isMarried = confirm (" Bạn đã hiểu bài chưa?")
+console.log(isMarried);
