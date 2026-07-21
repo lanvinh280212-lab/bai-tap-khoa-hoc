79e339ec8a9de01268bf21cb490a44a7745b86ab
Nguyen Thi Lan <nguyenlan280204@gmail.com>
2026-07-21 11:57:19 +0700

    them bai 8

---
+++ moudle2/JS_Fundamentals_Flowchart/bai8/script.js
@@ -0,0 +1,9 @@
+let myUniversity = " Đại học Hòa Bình";
+let studentName = " Nguyễn Thị Lan";
+let studentAge = 21;
+let isProgrammingFun= true;
+
+console.log("Trường:",myUniversity);
+console.log("Tên Sinh viên: ",studentName);
+console.log("Tuổi: ",studentAge);
+console.log("Học lập trình có vui không? ",isProgrammingFun);
