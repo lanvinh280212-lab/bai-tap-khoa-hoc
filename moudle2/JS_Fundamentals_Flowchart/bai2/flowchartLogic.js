34c1efb561a4332c2b4e79dbbc7c18c5a1f2e069
Nguyen Thi Lan <nguyenlan280204@gmail.com>
2026-07-21 10:23:55 +0700

    them bai 2

---
+++ moudle2/JS_Fundamentals_Flowchart/bai2/flowchartLogic.js
@@ -0,0 +1,24 @@
+let chuyenCan = 85;
+let diemTB = 6.0;
+let giayPhep = flase;
+let dieuKienHocLuc = (chuyenCan > 80 && diemTB >=5);
+if (dieuKienHocLuc) {
+    console.log("Đạt điều kiện về điểm và chuyên cần");
+}
+else {
+    console.log("Không đạt điều kiện về điểm và chuyên cần");
+}
+
+if (dieuKienHocLuc || giayphep) {
+    console.log("Kết quả: Sinh viên đủ điều kiện dự thi.");
+}
+else {
+    console.log("Kết quả: Sinh viên không đủ điều kiện dự thi.");
+}
+
+if (!giayPhep) {
+    console.log(" Sinh viên này Không có giấy phép đặt biệt.");
+}
+else {
+    console.log(" Sinh viên này Có giấy phép đặt biệt.");
+}
